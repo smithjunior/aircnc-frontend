9dffb24b4f2351de530b3d9bbb919d6473a469b7
Smith Júnior <smith.junior@icloud.com>
2019-10-13 23:48:48 -0300

    Login static page created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './App.css'
-
+import logo from './assets/logo.svg'
 function App() {
-  return (<div className='App'>
-    <header className='App-header' />
-  </div>)
+  return (
+    <div className='container'>
+      <img src={logo} alt='Aircnc' />
+      <div className='content'>
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa!
+        </p>
+        <form action=''>
+          <label htmlFor='email'>E-MAIL *</label>
+          <input
+            type='email'
+            id='emaisl'
+            placeholder='Seu melhor email' />
+          <button type='submit' className='btn'> Entrar</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default App
