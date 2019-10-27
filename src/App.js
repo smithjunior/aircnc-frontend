b5d85b4afe77882060b6527f840507eca0202a2c
Smith Júnior <smith.junior@icloud.com>
2019-10-26 21:39:56 -0300

    Management spots

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react'
+import Routes from './routes'
+
 import './App.css'
 import logo from './assets/logo.svg'
-function App() {
+
+function App () {
   return (
     <div className='container'>
       <img src={logo} alt='Aircnc' />
       <div className='content'>
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa!
-        </p>
-        <form action=''>
-          <label htmlFor='email'>E-MAIL *</label>
-          <input
-            type='email'
-            id='emaisl'
-            placeholder='Seu melhor email' />
-          <button type='submit' className='btn'> Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   )
