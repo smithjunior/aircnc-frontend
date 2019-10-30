1e5f428e3e953cd2e2a1ce153f0e77054bfc9fd3
Smith Júnior <smith.junior@icloud.com>
2019-10-30 11:00:32 -0300

    Removed Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Router } from 'react-router-dom'
 import Routes from './routes'
-import history from './services/history'
 
 import './App.css'
 import logo from './assets/logo.svg'
@@ -11,9 +9,7 @@ function App() {
     <div className='container'>
       <img src={logo} alt='Aircnc' />
       <div className='content'>
-        <Router history={history}>
-          <Routes />
-        </Router>
+        <Routes />
       </div>
     </div>
   )
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,11 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import New from './pages/New'
+import history from './services/history'
 
 export default function Routes() {
   return (
-    <BrowserRouter>
+    <BrowserRouter history={history}>
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/dashboard" exact component={Dashboard} />
